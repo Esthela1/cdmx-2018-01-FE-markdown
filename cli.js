b0ad741b6470f9e9db4fd1f2a1baba8d8696568c
Esthela1 <39842176+Esthela1@users.noreply.github.com>
2018-09-10 05:54:11 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
-/******************
-invoca funcion que lee el archivo md */
-const readReadme = require ('./index.js'); 
+/* secuencia de caracteres shebang*/
+/******************/
 
-/******************
-invoca funcion que cuenta las lineas del archivo md */
-const countLines = require ('./index.js'); 
 
-const mdLinks = require ('./index.js'); 
-
-const statusLinks = require ('./index.js'); 
