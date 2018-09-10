7a817290c8690d8d928906ca088e2591d72b2e0f
Esthela1 <39842176+Esthela1@users.noreply.github.com>
2018-09-10 05:40:28 -0500

    añadir cli.js

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/******************
+invoca funcion que lee el archivo md */
+const readReadme = require ('./index.js'); 
+
+/******************
+invoca funcion que cuenta las lineas del archivo md */
+const countLines = require ('./index.js'); 
+
+const mdLinks = require ('./index.js'); 
+
+const statusLinks = require ('./index.js'); 
