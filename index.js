204f55bea921ef254662db0568c83b23b2cb90c1
Esthela1 <39842176+Esthela1@users.noreply.github.com>
2018-09-10 05:55:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Primer commit
+// funcion para leer el readme
+const fs = require('fs');
+const readReadme = (callback) => {
+  fs.readFile('./README.md', 'utf8', (err, data)=>{
+    if (err) {
+      console.log('Tienes un error');
+    } else {
+      link(data);
+    }
+  });
+};  
+readReadme();
+
